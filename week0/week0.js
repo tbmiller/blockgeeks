3fa093e7175a311eee7c928e6fd91e6c3e363c71
Travis Miller <travis@spark.us>
2018-10-29 20:32:22 -0400

    Week 0 Assessment - Added Assessment #1

--- week0/week0.js
+++ week0/week0.js
@@ -1,6 +1,53 @@
-var repeatNumbers = function(data) {
-  // Put your solution here
+/*
+
+Travis Miller
+Week 0 Assessmsnt 
+JavaScript Primer
+
+*/
+
+// CORE FUNCTION
+
+var repeatNumbers = function(data_sample) {
+  
+  // Variable Establishment
+   var value_to_repeat;
+   var times_to_repeat;
+   var concat_result = "";
+  
+  
+// Initial Loop to Get Access to  data samples in 2D Array
+  for(var i = 0; i < data_sample.length; i++) {
+    
+    
+      // Extract and assign varibales from each array property
+      value_to_repeat = data_sample[i][0];
+      times_to_repeat = data_sample[i][1];
+    
+    // Prepare string for concatenated display
+    concat_result = concat_result + " ";
+    
+    // Second Loop to use the Number of times to explain
+    for(var n = 0; n < times_to_repeat; n++) {
+      
+      // Build Concatenated Result
+      concat_result += value_to_repeat;
+      
+            
+    }
+    
+    // Output concatenated result
+     console.log(concat_result);
+
+    
+    }
+  
+  //return false;
+  
+  
 };
+
+// DATA SAMPLES
 
 console.log(repeatNumbers([[1, 10]]));
 console.log(repeatNumbers([[1, 2], [2, 3]]));
